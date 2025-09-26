67dbc6a54ee5e6f35005e840f7e64317b531d0df
Theeojeong <wogusto13@gmail.com>
2025-09-26 23:20:43 +0900

    ChatGPT Clone Multimodal 기능 추가

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -3,6 +3,7 @@ load_dotenv()
 
 from openai import OpenAI
 import streamlit as st
+import base64
 from agents import Agent, SQLiteSession, Runner, WebSearchTool, FileSearchTool
 import asyncio
 
@@ -20,12 +21,13 @@ if "agent" not in st.session_state:
             - Web Search Tool: Use this when the user asks a questions that isn't in your training data. Use this tool when the users asks about current or future events, when you think you don't know the answer, try searching for it in the web first.
             - File Search Tool: Use this tool when the user asks a question about facts related to themselves. Or when they ask questions about specific files. 파일에 명시되어 있지 않은 정보는 웹서치 도구를 활용하세요.
         """,
-        tools=[WebSearchTool(), FileSearchTool(
+        tools=[
+            WebSearchTool(), 
+            FileSearchTool(
             vector_store_ids=[VECTOR_STORE_ID],
             max_num_results=3
         )]
     )
-
 agent = st.session_state["agent"]
 
 if "session" not in st.session_state:
@@ -47,7 +49,13 @@ async def paint_history():
         if "role" in message:
             with st.chat_message(message["role"]):
                 if message["role"] == "user":
-                    st.write(message['content'])
+                    content = message["content"]
+                    if isinstance(content, str):
+                        st.write(content)
+                    elif isinstance(content, list):
+                        for part in content:
+                            if "image_url" in part:
+                                st.image(part["image_url"])
                 else:
                     if message['type'] == "message": 
                         st.write(message['content'][0]['text'])
@@ -84,6 +92,7 @@ async def run_agent(message):
         status_container = st.status("⏳", expanded=False)
         text_placeholder = st.empty()
         response = ""
+
         stream = Runner.run_streamed(
             agent,
             message,
@@ -92,6 +101,7 @@ async def run_agent(message):
 
         async for event in stream.stream_events():
             if event.type == "raw_response_event":
+
                 update_status(status_container, event.data.type)
 
                 if event.data.type == "response.output_text.delta":
@@ -99,10 +109,15 @@ async def run_agent(message):
                     text_placeholder.write(response)
 
 prompt = st.chat_input(
-    "Agent에게 질문해주세요. 파일은 '.txt' 형식만 지원합니다.",
+    "Agent에게 질문해주세요. 이미지 업로드 가능",
     accept_file=True,
-    file_type=["txt"]
-    )
+    file_type=[
+        "txt",
+        "jpg",
+        "jpeg",
+        "png"
+    ]
+)
 
 if prompt:
     for file in prompt.files:
@@ -119,6 +134,31 @@ if prompt:
                         file_id=uploaded_file.id
                     )
                     status.update(label="✅ File uploaded", state="complete")
+        elif file.type.startswith("image/"):
+            with st.chat_message('ai'):
+                with st.status("⏳ Uploading Image...") as status:
+                    file_byte = file.getvalue()
+                    base64_data = base64.b64encode(file_byte).decode("utf-8")
+                    data_url = f"data:{file.type};base64,{base64_data}"
+                    asyncio.run(
+                        session.add_items(
+                            [
+                                {
+                                    "role": "user",
+                                    "content": [
+                                        {
+                                            "type": "input_image",
+                                            "detail": "auto",
+                                            "image_url": data_url
+                                        }
+                                    ]
+                                }
+                            ]
+                        )
+                    )
+                    status.update(label="✅ Image uploaded", state="complete")
+                with st.chat_message("user"):
+                    st.image(data_url)
     if prompt.text:
         with st.chat_message('user'):
             st.write(prompt.text)
