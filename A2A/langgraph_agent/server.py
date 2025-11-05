2e1382f7c8e53432e34493d3f4acc8eaa6137cc7
Theeojeong <wogusto13@gmail.com>
2025-11-05 23:03:51 +0900

    update deployment and create claude-agent-sdk

--- A2A/langgraph_agent/server.py
+++ A2A/langgraph_agent/server.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
+import logging
 from graph import graph
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
 
 
-def hadle_message():
+def hadle_message(message: str):
 
-    graph.invoke()
+    response = graph.invoke({"messages": [{"role": "user", "content": message}]})
+
+    return response['messages'][-1].content
 
 
 @app.get("/.well-known/agent-card.json")
@@ -40,7 +45,25 @@ def get_agent_card():
 async def handle_message(req: Request):
     body = await req.json()
     body2 = body.get("params").get("message").get("parts")
-    print(body2)
+    body2.reverse()
+
+    message_texts = ""
+
+    for message in body2:
+        text = message.get('text')
+        message_texts += f"{text}\n"
+
+    result = hadle_message(message_texts)
+
     return {
-        "return": body
+        "id": "message_1",
+        "jsonrpc": "2.0",
+        "result": {
+            "kind": "message",
+            "message_id": "239827493847289374",
+            "role": "agent",
+            "parts": [
+                {"kind": "text", "text": result},
+            ],
+        },
     }
--- deployment/main.py
+++ deployment/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from deployment!")
+from agents import Agent, Runner
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from openai import AsyncOpenAI
+from pydantic import BaseModel
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+agent = Agent(name="Assistant", instructions="You help users with their questions.")
+
+class CreateConversationResponse(BaseModel):
+    conversation_id: str
+
+app = FastAPI()
+
+client = AsyncOpenAI()
+
+
+@app.post("/conversation")
+async def create_conversation_id() -> CreateConversationResponse:
+
+    conversation = await client.conversations.create()
+
+    return {
+        "conversation_id": conversation.id
+    }
